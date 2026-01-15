05e6f110612477a518a9550c4c387b849416fd6e
bthos <bthos@example.com>
2026-01-15 21:54:33 +0100

    chore: update build and release workflows for new bundle path
    - Changed the output path of the bundle file to 'www/community/octopus_energy_es/octopus-consumption-card.bundle.js'.
    - Updated the .gitignore to reflect the new bundle path.
    - Modified build and release workflows to create the necessary directory structure and verify the new bundle location.
    - Added steps to copy the bundle to the root for HACS compatibility during the build process.

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ const VERSION = packageJson.version;
 export default {
   input: 'dist/octopus-consumption-card.js',
   output: {
-    file: 'octopus-consumption-card.bundle.js',
+    file: 'www/community/octopus_energy_es/octopus-consumption-card.bundle.js',
     format: 'iife',
     name: 'OctopusConsumptionCard',
     sourcemap: false,  // Disable source maps for production builds
